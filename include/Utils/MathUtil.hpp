4a19eee66fee7966de2fd20daf0c71132cb145a4
Arthur Maurer <arthimaurer@gmail.com>
2015-10-13 06:18:58 +0200

    MathUtils random range

--- include/Utils/MathUtil.hpp
+++ include/Utils/MathUtil.hpp
@@ -2,6 +2,11 @@
 
 class MathUtil
 {
+	static bool			randSeedSet;
+
 public:
 	static float	degToRad(float deg);
+
+	static float	random(float min, float max, float precision = 1.f);
+	static int		random(int min, int max);
 };
--- src/Utils/MathUtil.cpp
+++ src/Utils/MathUtil.cpp
@@ -1,12 +1,46 @@
 
 #include "Utils/MathUtil.hpp"
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
 
 #ifndef M_PI
 # define M_PI	3.14159265358979323846
 #endif
 
+bool	MathUtil::randSeedSet = false;
+
 float	MathUtil::degToRad(float deg)
 {
 	return (deg * (static_cast<float>(M_PI) / 180.0f));
+}
+
+float		MathUtil::random(float min, float max, float precision)
+{
+	float	r;
+	float	invPrecision = 1.f / precision;
+
+	if (!randSeedSet)
+	{
+		randSeedSet = true;
+		srand(static_cast<unsigned>(time(NULL)));
+	}
+
+	r = static_cast<float>(rand()) / invPrecision;
+	r = fmodf(r, (max - min));
+	r += min;
+	r = std::trunc(invPrecision * r) / invPrecision;
+
+	return r;
+}
+
+int		MathUtil::random(int min, int max)
+{
+	if (!randSeedSet)
+	{
+		randSeedSet = true;
+		srand(static_cast<unsigned>(time(NULL)));
+	}
+
+	return (rand() % (max - min) + min);
 }
