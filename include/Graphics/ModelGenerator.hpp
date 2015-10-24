41996d66d19dbbfe18a1182ca7f975364a150721
Arthur Maurer <arthimaurer@gmail.com>
2015-10-24 09:37:18 +0200

    Cube shape added to ModelGenerator

--- include/Graphics/ModelGenerator.hpp
+++ include/Graphics/ModelGenerator.hpp
@@ -2,14 +2,15 @@
 #ifndef _MODEL_GENERATOR_HPP
 # define _MODEL_GENERATOR_HPP
 
-# include "Vec2.hpp"
-
+class Vec2;
+class Vec3;
 class Model;
 
 class ModelGenerator
 {
 public:
 	static Model &	generatePlane(const Vec2 & size);
+	static Model &	generateCube(const Vec3 & size);
 };
 
 #endif /* _MODEL_GENERATOR_HPP */
--- src/Graphics/ModelGenerator.cpp
+++ src/Graphics/ModelGenerator.cpp
@@ -7,11 +7,13 @@
 Model &		ModelGenerator::generatePlane(const Vec2 & size)
 {
 	RawModelData	modelData;
+	float			with2 = size.x / 2.f;
+	float			height2 = size.y / 2.f;
 
-	modelData.positions.push_back(Vec3(0, 0, 0));
-	modelData.positions.push_back(Vec3(size.x, 0, 0));
-	modelData.positions.push_back(Vec3(size.x, size.y, 0));
-	modelData.positions.push_back(Vec3(0, size.y, 0));
+	modelData.positions.push_back(Vec3(-with2, -height2, 0));
+	modelData.positions.push_back(Vec3(with2, -height2, 0));
+	modelData.positions.push_back(Vec3(with2, height2, 0));
+	modelData.positions.push_back(Vec3(-with2, height2, 0));
 
 	modelData.uvs.push_back(Vec2(0, 0));
 	modelData.uvs.push_back(Vec2(1.f, 0));
@@ -29,6 +31,85 @@ Model &		ModelGenerator::generatePlane(const Vec2 & size)
 	modelData.indices.push_back(2);
 	modelData.indices.push_back(3);
 	modelData.indices.push_back(0);
+
+	return ModelManager::createModel(modelData);
+}
+
+Model &		ModelGenerator::generateCube(const Vec3 & size)
+{
+	RawModelData	modelData;
+	float			with2 = size.x / 2.f;
+	float			height2 = size.y / 2.f;
+	float			depth2 = size.z / 2.f;
+
+	modelData.positions.push_back(Vec3(-with2, height2, depth2));
+	modelData.positions.push_back(Vec3(with2, height2, depth2));
+	modelData.positions.push_back(Vec3(with2, -height2, depth2));
+	modelData.positions.push_back(Vec3(-with2, -height2, depth2));
+	modelData.positions.push_back(Vec3(-with2, height2, -depth2));
+	modelData.positions.push_back(Vec3(with2, height2, -depth2));
+	modelData.positions.push_back(Vec3(with2, -height2, -depth2));
+	modelData.positions.push_back(Vec3(-with2, -height2, -depth2));
+
+	modelData.uvs.push_back(Vec2(0, 0));
+	modelData.uvs.push_back(Vec2(1.f, 0));
+	modelData.uvs.push_back(Vec2(1.f, 1.f));
+	modelData.uvs.push_back(Vec2(0, 1.f));
+	modelData.uvs.push_back(Vec2(0, 0));
+	modelData.uvs.push_back(Vec2(1.f, 0));
+	modelData.uvs.push_back(Vec2(1.f, 1.f));
+	modelData.uvs.push_back(Vec2(0, 1.f));
+
+	modelData.normals.push_back(Vec3(-1.f, 1.f, 1.f));
+	modelData.normals.push_back(Vec3(1.f, 1.f, 1.f));
+	modelData.normals.push_back(Vec3(1.f, -1.f, 1.f));
+	modelData.normals.push_back(Vec3(-1.f, -1.f, 1.f));
+	modelData.normals.push_back(Vec3(-1.f, 1.f, -1.f));
+	modelData.normals.push_back(Vec3(1.f, 1.f, -1.f));
+	modelData.normals.push_back(Vec3(1.f, -1.f, -1.f));
+	modelData.normals.push_back(Vec3(-1.f, -1.f, -1.f));
+
+	modelData.indices.push_back(2);
+	modelData.indices.push_back(1);
+	modelData.indices.push_back(0);
+	modelData.indices.push_back(0);
+	modelData.indices.push_back(3);
+	modelData.indices.push_back(2);
+
+	modelData.indices.push_back(4);
+	modelData.indices.push_back(5);
+	modelData.indices.push_back(6);
+	modelData.indices.push_back(6);
+	modelData.indices.push_back(7);
+	modelData.indices.push_back(4);
+
+	modelData.indices.push_back(1);
+	modelData.indices.push_back(5);
+	modelData.indices.push_back(4);
+	modelData.indices.push_back(4);
+	modelData.indices.push_back(0);
+	modelData.indices.push_back(1);
+
+	modelData.indices.push_back(6);
+	modelData.indices.push_back(2);
+	modelData.indices.push_back(3);
+	modelData.indices.push_back(3);
+	modelData.indices.push_back(7);
+	modelData.indices.push_back(6);
+
+	modelData.indices.push_back(3);
+	modelData.indices.push_back(0);
+	modelData.indices.push_back(4);
+	modelData.indices.push_back(4);
+	modelData.indices.push_back(7);
+	modelData.indices.push_back(3);
+
+	modelData.indices.push_back(6);
+	modelData.indices.push_back(5);
+	modelData.indices.push_back(1);
+	modelData.indices.push_back(1);
+	modelData.indices.push_back(2);
+	modelData.indices.push_back(6);
 
 	return ModelManager::createModel(modelData);
 }
